55357fe9383351da0910869405b0e78601ccd673
jsnellen1394 <98684883+jsnellen1394@users.noreply.github.com>
2022-01-30 16:00:01 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,23 @@
 /**
  *
- * @author Your Name
+ * jsnellen 
  */
 
 public class HelloWorld {
 
     public static void main(String[] args) {
-        Class c1 = new Class1();
-        Class c2 = new Class2();
+        Class Class1 = new Class1();
+        Class Class2 = new Class2();
         System.out.println("Hello, World!");
     }
     private void f1() {
         // v1.0
     }
 
+    private void f2() {
+        // v2.0
+    }
+    private void f3() {
+        // v3.0
+    }
 }
